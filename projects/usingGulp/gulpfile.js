18f745be8e2a716c94ddc4467dad91c7a630f44d
christian <christiancasey969@gmail.com>
2017-08-18 21:16:27 +0100

    Gulp tasks including sass compiling

--- projects/usingGulp/gulpfile.js
+++ projects/usingGulp/gulpfile.js
@@ -1,5 +1,6 @@
-
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
 
 
 
@@ -11,3 +12,45 @@ const gulp = require('gulp');
   gulp.watch - Watch files and folders for changes
 
 */
+
+// LOGS MESSAGE
+
+gulp.task('message', function() {
+  return console.log('Gulp is running...');
+});
+
+// COPY ALL HTML FILES
+
+gulp.task('copyHtml', function() {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+})
+
+// OPTIMISE IMAGES
+
+gulp.task('imageMin', () =>
+  gulp.src('src/images/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+);
+
+// COMPILE SASS
+
+gulp.task('sass', function(){
+  gulp.src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+})
+
+// GULP TASKS ALTOGETHER
+
+gulp.task('default', ['message', 'copyHtml', 'imageMin', 'sass']);
+
+// GULP WATCH
+
+gulp.task('watch', function(){
+  gulp.watch();
+  gulp.watch('src/images/*', ['imageMin']);
+  gulp.watch('src/sass/*.scss', ['sass']);
+  gulp.watch('src/*.html', ['copyHtml']);
+});
