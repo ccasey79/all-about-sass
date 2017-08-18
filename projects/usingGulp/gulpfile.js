8775c3ccd3534f8fe813fd65330338d935731ffe
christian <christiancasey969@gmail.com>
2017-08-18 15:22:31 +0100

    Gulp installed globally, locally - basic structure

---
+++ projects/usingGulp/gulpfile.js
@@ -0,0 +1,13 @@
+
+const gulp = require('gulp');
+
+
+
+/* TOP LEVEL FUNCTIONS TASKS
+
+  gulp.task - Define tasks
+  gulp.src - Point to files to use
+  gulp.dest - Points to folder to output
+  gulp.watch - Watch files and folders for changes
+
+*/
